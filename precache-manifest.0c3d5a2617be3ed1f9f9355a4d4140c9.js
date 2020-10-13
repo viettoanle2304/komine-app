2d55e96a3a45331611c24d9521990edf928f902d
Viet Toan Le <viettoanle2304@gmail.com>
2020-10-14 01:57:41 +0300

    Updates

---
+++ precache-manifest.0c3d5a2617be3ed1f9f9355a4d4140c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63d636e24e6cfa353d2e850efefa255",
+    "url": "/viettoanle2304/komine-app/index.html"
+  },
+  {
+    "revision": "18733433991e57b7f068",
+    "url": "/viettoanle2304/komine-app/static/css/main.61fb0ede.chunk.css"
+  },
+  {
+    "revision": "f3d81edd7e8372fb1176",
+    "url": "/viettoanle2304/komine-app/static/js/2.509d9487.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/viettoanle2304/komine-app/static/js/2.509d9487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18733433991e57b7f068",
+    "url": "/viettoanle2304/komine-app/static/js/main.9b30910d.chunk.js"
+  },
+  {
+    "revision": "b1d5638e2b631c499960",
+    "url": "/viettoanle2304/komine-app/static/js/runtime-main.1e430de2.js"
+  }
+]);
