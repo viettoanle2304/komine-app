523709edd5873d9c518027b945e427f3d7bf7163
Viet Toan Le <viettoanle2304@gmail.com>
2020-10-17 17:57:52 +0300

    Manga Item Update

--- src/components/manga-lists/MangaItems.js
+++ src/components/manga-lists/MangaItems.js
@@ -5,8 +5,11 @@ export const MangaItems = [
         text: "As a child, Monkey D. Luffy was inspired to become a pirate by listening to the tales of the buccaneer \"Red-Haired\" Shanks. But Luffy's life changed when he accidentally ate the Gum-Gum Devil Fruit and gained the power to stretch like rubber...at the cost of never being able to swim again! Years later, still vowing to become the king of the pirates, Luffy sets out on his adventure...one guy alone in a rowboat, in search of the legendary  \"One Piece,\" said to be the greatest treasure in the world...",
         genres: 'Adventure, Fantasy',
         chaps: [
-            '204',
-            '203'
+            '992',
+            '991',
+            '990',
+            '889',
+            '888'
         ],
         cover_image: '/images/OnePieceTitle.png',
         poster_images: '',
@@ -27,7 +30,104 @@ export const MangaItems = [
         ],
         cover_image: '/images/NarutoTitle.jpg',
         poster_image: '/images/NarutoPoster.jpg',
+        week_rank: '5',
+        month_rank: '4'
+    },
+    {
+        name: 'One Punch Man',
+        path: '/manga-info?Name=One-Punch-Man',
+        text: "One-Punch Man (Japanese: ワンパンマン, Hepburn: Wanpanman) is a Japanese superhero franchise created by the artist ONE. It tells the story of Saitama, a superhero who can defeat any opponent with a single punch but seeks to find a worthy foe after growing bored by a lack of challenge due to his overwhelming strength.",
+        genres: 'Action, Comedy',
+        chaps: [
+            '181',
+            '180',
+            '179',
+            '178',
+            '177'
+        ],
+        cover_image: '/images/OnePunchManTitle.jpg',
+        poster_images: '/',
         week_rank: '2',
+        month_rank: '3'
+    },
+    {
+        name: 'Black Clover',
+        path: '/manga-info?Name=Black-Clover',
+        text: "Black Clover (Japanese: ブラッククローバー, Hepburn: Burakku Kurōbā) is a Japanese manga series written and illustrated by Yūki Tabata. The story centers around Asta, a young boy seemingly born without any magic power, something that is unknown in the world he lives in. With his fellow mages from the Black Bulls, Asta plans to become the next Wizard King.",
+        chaps: [
+            '268',
+            '267',
+            '266',
+            '265',
+            '264'
+        ],
+        cover_image: '/images/BlackCloverTitle.jpg',
+        poster_images: '/',
+        week_rank: '3',
+        month_rank: '5'
+    },
+    {
+        name: 'Enen no Shouboutai',
+        path: '/manga-info?Name=Enen-no-shouboutai',
+        text: "In Year 198 of the Solar Era in Tokyo, Special Fire Force's are fighting against a phenomenon called Spontaneous Human Combustion where humans beings are turned into living infernos called “Infernals.” While the Infernals are First Generation cases of spontaneous human combustion, later generations possess the ability to manipulate flames while retaining human form. Shinra Kusakabe, a youth who gained the nickname Devil’s Footprints for his ability to ignite his feet at will, joins the Special Fire Force Company 8 which composes of other flames users as they work to extinguish any Infernals they encounter. As a faction that is creating Infernals appears, Shinra begins to uncover the truth behind a mysterious fire that caused the death of his family twelve years ago.",
+        chaps: [
+            '202',
+            '201',
+            '200',
+            '199',
+            '198'
+        ],
+        cover_image: '/images/EnenNoShouboutaiTitle.png',
+        poster_images: '/',
+        week_rank: '4',
         month_rank: '1'
+    },
+    {
+        name: 'Mairimashita Iruma-kun',
+        path: '/manga-info?Name=Mairimashita-Iruma-kun',
+        text: "Sullivan is questioned by Demon Border Control. Most of the first years panic at the thought of their parents visiting the school. Sullivan appoints Naberius as temporary school director while the rest of the staff wonders what Sullivan did to get arrested. Kirio reveals that all Battlers give presentations competing for prizes, maybe even a rise in rank, but the Magical Apparatus Battler has such a small budget they normally stand no chance, until Iruma is inspired to create fireworks. As the testing requires darkness Kirio decides they must stay at school overnight. While trying to create gunpowder Kirio reveals he has an ability to create magical barriers, the only reason he survives so many explosions. Iruma asks Amelie to loan him one of her manga so Kirio can see what a firework should look like, but she refuses, at least until she realises she would get to see real fireworks. After they successfully create fireworks they exhaust themselves with a pillow fight, utilising Iruma's dodge ability and Kirio's barriers. Kirio is later contacted by a powerful demon that can use lightning magic, revealing he is the reason Sullivan has been arrested and he has been plotting with Kirio to create a machine to destroy the school.",
+        chaps: [
+            '175',
+            '174',
+            '173',
+            '172',
+            '171'
+        ],
+        cover_image: '/images/MairimashitaIrumaKunTitle.png',
+        poster_images: '/',
+        week_rank: '6',
+        month_rank: '8'
+    },
+    {
+        name: 'Kimetsu no Yaiba',
+        path: '/manga-info?Name=Kimetsu-no-Yaiba',
+        text: "Demon Slayer: Kimetsu no Yaiba (Japanese: 鬼滅の刃, Hepburn: Kimetsu no Yaiba, lit. \"Blade of Demon Destruction\") is a Japanese manga series written and illustrated by Koyoharu Gotōge. It follows Tanjiro Kamado, a young boy who becomes a demon slayer after his family is slaughtered and his younger sister Nezuko is turned into a demon.",
+        chaps: [
+            '205',
+            '204',
+            '203',
+            '202',
+            '201'
+        ],
+        cover_image: '/images/KimetsuNoYaibaTitle.jpg',
+        poster_images: '/',
+        week_rank: '7',
+        month_rank: '6'
+    },
+    {
+        name: 'Dr.Stone',
+        path: '/manga-info?Name=Dr-Stone',
+        text: "In the modern world, every human on the planet was turned into stone after a mysterious flash of light. This manga tells the story of how some people from the Pre-Petrification World try to rebuild civilization in the Petrification Age, the Stone World. One of the main characters' first creations undoes the petrification and is called the stone formula.",
+        chaps: [
+            '168',
+            '167',
+            '166',
+            '165',
+            '164'
+        ],
+        cover_image: '/images/DrStoneTitle.jpg',
+        poster_images: '/',
+        week_rank: '8',
+        month_rank: '7'
     }
 ]
